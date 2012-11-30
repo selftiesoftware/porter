948483b96144a30ad1a4a2f686db796e95382fa0
Oleegholm <oleegholm@gmail.com>
2012-11-30 12:19:52 +0100

    started implementing an improved DXF import logic

---
+++ src/com/siigna/porter/DXF/DXFImport.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2012. Siigna is released under the creative common license by-nc-sa. You are free
+ * to Share — to copy, distribute and transmit the work,
+ * to Remix — to adapt the work
+ *
+ * Under the following conditions:
+ * Attribution —  You must attribute the work to http://siigna.com in the manner specified by the author or licensor (but not in any way that suggests that they endorse you or your use of the work).
+ * Noncommercial — You may not use this work for commercial purposes.
+ * Share Alike — If you alter, transform, or build upon this work, you may distribute the resulting work only under the same or similar license to this one.
+ */
+
+package com.siigna.porter.DXF
+
+import com.siigna.module.Module
+import com.siigna._
+
+/**
+ * An import module using Kabeja 0.4 DXF import JAR library.
+ * Kabeja version 0.4 is released under the apache 2.0 license.
+ * See the Kabeja 0.4 licence for more information.
+ */
+class DXFImport extends Module{
+
+  val stateMap : StateMap = Map(
+    'Start -> {
+      case _ => {
+        println("RUNNING PORTER")
+      }
+    }
+  )
+}
